bce2d20ec0ea20e554367399f53d40a038b0b216
Darknico <info@darknico.com>
2021-10-18 15:27:31 +0200

    Review constructors

--- poweroff-button-on-topbar@darknico.com/README.md
+++ poweroff-button-on-topbar@darknico.com/README.md
@@ -15,7 +15,9 @@ Requirements
 
 Changelog
 ==============
+- v3 (18-10-2021)
+  - Review constructors
 - v2 (15-10-2021)
-- Fix issue with 3.38
+  - Fix issue with 3.38
 - v1 (04-04-2020)
-- First Commit with base functionality
+  - First Commit with base functionality
--- poweroff-button-on-topbar@darknico.com/extension.js
+++ poweroff-button-on-topbar@darknico.com/extension.js
@@ -9,35 +9,45 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const Util = imports.misc.util;
 
-let text, button, icon;
+let icon = null;
+let button = null;
 
-class Extension {
-    constructor() {
+function init() {
 
-        icon = new St.Icon({
-            icon_name: 'system-shutdown',
-            style_class: 'system-status-icon'
-        });
+}
 
-        button = new St.Bin({
-            style_class: 'panel-button',
-            reactive: true,
-            can_focus: true,
-            track_hover: true
-        });
+function enable() {
 
-        button.set_child(icon);
-        button.connect('button-press-event', _poweroff);
+    icon = new St.Icon({
+        icon_name: 'system-shutdown',
+        style_class: 'system-status-icon'
+    });
+
+    button = new St.Bin({
+        style_class: 'panel-button',
+        reactive: true,
+        can_focus: true,
+        track_hover: true
+    });
+
+    button.set_child(icon);
+    button.connect('button-press-event', _poweroff);
+
+    Main.panel._rightBox.insert_child_at_index(button, -1);
+}
+
+function disable() {
+    Main.panel._rightBox.remove_child(button);
+    if (icon) {
+        icon.destroy()
+        icon = null;
     }
-
-    enable() {
-        Main.panel._rightBox.insert_child_at_index(button, -1);
-    }
-
-    disable() {
-        Main.panel._rightBox.remove_child(button);
+    if (button) {
+        button.destroy();
+        button = null;
     }
 }
+
 
 function _poweroff() {
     try {
@@ -47,6 +57,3 @@ function _poweroff() {
     }
 }
 
-function init() {
-    return new Extension();
-}
